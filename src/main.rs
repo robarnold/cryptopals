d709cd605fe5224372cfd316628aff610d3aad19
Rob Arnold <robarnold@cs.cmu.edu>
2018-05-06 16:01:58 -0700

    Use u8::from_str_radix

--- src/main.rs
+++ src/main.rs
@@ -68,13 +68,11 @@ fn set_one_challenge_one() {
 }
 
 fn parse_byte_string(s: &str) -> Vec<u8> {
-  let chars: Vec<char> = s.chars().collect();
   let mut v = Vec::with_capacity(s.len() / 2);
-  for i in 0..chars.len()/2 {
-    let c1 = chars[2 * i + 0].to_digit(16).unwrap();
-    let c2 = chars[2 * i + 1].to_digit(16).unwrap();
-    let b = (c1 << 4 | c2) as u8;
-    v.push(b);
+  let mut i = 0;
+  while i < s.len() {
+    v.push(u8::from_str_radix(&s[i..i+2], 16).unwrap());
+    i += 2;
   }
   v
 }
