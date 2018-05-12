a35823c177cb5e980a08601f9b701f60f0b3260c
Rob Arnold <robarnold@cs.cmu.edu>
2018-05-12 08:06:27 -0700

    Set 1 Challenge 5

--- src/main.rs
+++ src/main.rs
@@ -198,3 +198,22 @@ fn set_one_challenge_four() {
   assert_eq!(true, decoded_string.len() > 0);
   println!("{}", decoded_string);
 }
+
+fn xor_buffer_repeating(data: &[u8], key: &[u8]) -> Vec<u8> {
+  use std::ops::BitXor;
+  let mut v = Vec::with_capacity(data.len());
+  for i in 0..data.len() {
+    v.push(data[i].bitxor(key[i % key.len()]));
+  }
+  v
+}
+
+#[test]
+fn set_one_challenge_five() {
+  let poem = String::from("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal");
+  let key = String::from("ICE");
+  assert_eq!(
+    bytes!("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"),
+    xor_buffer_repeating(poem.as_bytes(), key.as_bytes())
+  );
+}
