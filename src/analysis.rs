042aa4dc27eee9b3ae5713a55ebbf0654aaabc4d
Rob Arnold <robarnold@cs.cmu.edu>
2018-05-13 15:12:39 -0700

    Implement hamming distance

--- src/analysis.rs
+++ src/analysis.rs
@@ -13,3 +13,19 @@ pub fn likely_plain_text_score(data: &[u8]) -> u32 {
   }
   (3 * alphabetic_chars + punctuation).saturating_sub(invalid_chars)
 }
+
+pub fn hamming_distance(a: &[u8], b: &[u8]) -> u32 {
+  use std::ops::BitXor;
+  let mut distance = 0;
+  for i in 0..a.len() {
+    distance += a[i].bitxor(b[i]).count_ones();
+  }
+  distance
+}
+
+#[test]
+fn s1c6_example() {
+  let s1 = "this is a test";
+  let s2 = "wokka wokka!!!";
+  assert_eq!(37, hamming_distance(s1.as_bytes(), s2.as_bytes()));
+}
