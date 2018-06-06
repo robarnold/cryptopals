a26237704d9aa16c1de158a2424b38aab693307d
Rob Arnold <robarnold@cs.cmu.edu>
2018-06-05 21:38:52 -0700

    Make `cargo fmt` happy

--- src/s2c12.rs
+++ src/s2c12.rs
@@ -5,8 +5,7 @@ fn challenge() {
   use oracle::Oracle;
   use rayon::prelude::*;
 
-  let secret_plaintext = 
-    b64::decode("Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK");
+  let secret_plaintext = b64::decode("Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK");
   let secret_plaintext_len = secret_plaintext.len();
   let mut o = oracle::AES128Append::new(secret_plaintext);
   let block_size = oracle::determine_block_size(&mut o);
